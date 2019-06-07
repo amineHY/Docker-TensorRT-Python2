f02db839f69ffea610ecedcb0e142d2eb15c644e
amine.hadjyoucef <amine.hadjyoucef@dronevolt.com>
2019-06-07 17:25:43 +0200

    edit

--- setup_docker.sh
+++ setup_docker.sh
@@ -7,5 +7,6 @@ elif [ "$1" == "push" ]; then
 	docker push aminehy/tensorrt:v1.2
 elif [ "$1" == "run" ]; then
 	echo "[info] Run the docker container"
+	xhost +
 	docker run -it --rm -v $HOME:$HOME -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY aminehy/tensorrt:v1.2
 fi
