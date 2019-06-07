d916a59db5cf2cbcb061959bf64991313f0f39c2
amine.hadjyoucef <amine.hadjyoucef@dronevolt.com>
2019-06-07 16:38:02 +0200

    edit setup file

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,5 +1,5 @@
 docker build -t aminehy/tensorrt:v1.2 .
 # docker push aminehy/tensorrt:v1.2
 
-docker run -it --rm aminehy/tensorrt:v1.2 
+docker run -it --rm -v $HOME:$HOME -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY aminehy/tensorrt:v1.2
 
